47e4bc41126e4709da24c30153048a32ff656a75
Chaeng-Ploypaphat-S <ploypaphat.saltz@gmail.com>
2026-05-25 12:17:16 -0700

    [learning]: test change lambda function

--- lib/aws-cdk-proj1-stack.ts
+++ lib/aws-cdk-proj1-stack.ts
@@ -2,6 +2,7 @@ import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { dirname, join } from 'path';
 
+import { CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 const __dirname = dirname(__filename);
@@ -10,11 +11,19 @@ export class AwsCdkProj1Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const handler = new Function(this, 'MyLambdaFunction', {
+    const handlerFun = new Function(this, 'MyLambdaFunction', {
       runtime: Runtime.NODEJS_20_X,
-      memorySize: 512,
+      memorySize: 128,
       handler: 'app.handler',
       code: Code.fromAsset(join(__dirname, '../lambdas')),
+      environment: {
+        NAME: 'Julia',
+        AGE: '30',
+      },
+    });
+
+    new CfnOutput(this, 'LambdaFunctionName', {
+      value: handlerFun.functionArn,
     });
   }
 }
